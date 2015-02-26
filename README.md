c5eea6468e272e16f9b425dbb5891a3eb8d1687d
David Burles <dburles@gmail.com>
2015-02-26 14:11:19 +1100

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+meteor-google-maps-demo
+==================
+
+A basic demo app for https://github.com/dburles/meteor-google-maps
---
+++ map-demo.js
@@ -0,0 +1,29 @@
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+
+  Template.body.helpers({
+    exampleMapOptions: function() {
+      // Make sure the maps API has loaded
+      if (GoogleMaps.loaded()) {
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
+
+  Template.body.created = function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('exampleMap', function(map) {
+      // Add a marker to the map once it's ready
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
+  };
+}
